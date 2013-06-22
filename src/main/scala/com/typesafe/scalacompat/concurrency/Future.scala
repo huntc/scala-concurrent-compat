b982c327ede488233386fc9dede0953d8d7d0aee
Christopher Hunt <huntchr@gmail.com>
2013-06-22 13:15:53 +1000

    Removed lines causing confusion - will be properly implemented once the DeferredResult interface evolves.

--- src/main/scala/com/typesafe/scalacompat/concurrency/Future.scala
+++ src/main/scala/com/typesafe/scalacompat/concurrency/Future.scala
@@ -1,22 +1,6 @@
 package com.typesafe.scalacompat.concurrency
 
-import java.util.concurrent.{ Executor, Callable }
-import com.typesafe.scalacompat.concurrent.DeferredResult
-
-class Future[T] {
-  val future: scala.concurrent.Future[T] = ???
-
-  def onSuccess(r: Callable[T]) {}
-
-  def onSuccess(r: Callable[T], executor: Executor) {}
-
-  def onFailure(r: Callable[T]) {}
-
-  def onFailure(r: Callable[T], executor: Executor) {}
-
-  def onComplete(r: Callable[T]) {}
-
-  def onComplete(r: Callable[T], executor: Executor) {}
-
-  def isCompleted: Boolean = ???
-}
+/**
+ * Scala's Future will be wrapped with a Future type here that extends DeferredResult.
+ * There will also be a wrapper for Scala's Promise.
+ */
