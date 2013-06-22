8182d980e33de4ebfa974a81b140420c808481b3
Christopher Hunt <huntchr@gmail.com>
2013-06-23 05:48:44 +1000

    More methods and the tidy-up of documentation

--- src/main/java/com/typesafe/scalacompat/concurrent/DeferredResult.java
+++ src/main/java/com/typesafe/scalacompat/concurrent/DeferredResult.java
@@ -3,8 +3,6 @@ package com.typesafe.scalacompat.concurrent;
 import com.typesafe.scalacompat.util.Optional;
 import com.typesafe.scalacompat.util.function.*;
 
-import java.util.concurrent.Callable;
-
 /**
  * A DeferredResult is a Future that can be observed. Actions are provided
  * to execute when the results of operations are available.
@@ -21,14 +19,14 @@ public interface DeferredResult<T> {
     /* Creational methods */
 
     /**
-     * Produces a result.
+     * Produces a future result.
      */
-    <T> DeferredResult<T> of(Callable<T> action);
+    <T> DeferredResult<T> of(Supplier<T> supplier);
 
     /**
-     * Produces a result of a sequence of actions.
+     * Produces a future result of a sequence of actions.
      */
-    <T> DeferredResult<T> of(Callable<T>... actions);
+    <T> DeferredResult<T> of(Supplier<T>... suppliers);
 
     /**
      * Creates an already completed result with the specified exception.
@@ -43,91 +41,85 @@ public interface DeferredResult<T> {
     /* Instance methods */
 
     /**
-     * When this result is completed successfully (i.e. with a value),
-     * apply the provided action to the value.
+     * When this future is completed successfully (i.e. with a value),
+     * apply the action function to the value.
      * <p/>
-     * If the result has already been completed with a value,
+     * If the future has already been completed with a value,
      * this will either be applied immediately or be scheduled asynchronously.
      */
     void onSuccess(Consumer<? super T> action);
 
     /**
-     * When this result is completed with a failure (i.e. with a throwable),
+     * When this future is completed with a failure (i.e. with a throwable),
      * apply the provided callback to the throwable.
      * <p/>
-     * If the result has already been completed with a failure,
+     * If the future has already been completed with a failure,
      * this will either be applied immediately or be scheduled asynchronously.
      * <p/>
-     * Will not be called in case that the result is completed with a value.
+     * Will not be called in case that the future is completed with a value.
      */
-    void onFailure(Consumer<? super Throwable> action);
+    void onFailure(Consumer<? super Throwable> failure);
 
     /**
-     * When this result is completed, either through an exception, or a value,
+     * When this future result is completed, either through an exception, or a value,
      * apply the provided action.
      * <p/>
-     * If the action has already been completed,
+     * If the future result has already been completed,
      * this will either be applied immediately or be scheduled asynchronously.
      */
     void onComplete(Runnable action);
 
+    /* Miscellaneous */
+
     /**
-     * Returns whether the result has already been completed with
+     * Returns whether the future has already been completed with
      * a value or an exception.
-     * <p/>
      *
-     * @return `true` if the result is already completed, `false` otherwise
+     * @return `true` if the future is already completed, `false` otherwise
      */
     boolean isCompleted();
 
     /**
-     * The value of this `result`.
-     * <p/>
-     * If the result is not completed the returned value will be empty.
-     * If the result is completed the value will be assigned.
+     * The value of this `Future`.
      */
     Optional<T> value();
 
     /* Projections */
 
     /**
-     * Returns a failed projection of this result.
+     * Returns a failed projection of this future.
      * <p/>
-     * The failed projection is a result holding a value of type `Throwable`.
+     * The failed projection is a future holding a value of type `Throwable`.
      * <p/>
-     * It is completed with a value which is the throwable of the original result
-     * in case the original result is failed.
+     * It is completed with a value which is the throwable of the original future
+     * in case the original future is failed.
+     * <p/>
+     * It is failed with a `NoSuchElementException` if the original future is completed successfully.
      */
     DeferredResult<Throwable> failed();
 
     /* Monadic operations */
 
     /**
-     * Asynchronously processes the value in the result once the value becomes available.
+     * Asynchronously processes the value in the future once the value becomes available.
      * <p/>
-     * Will not be called if the result fails.
+     * Will not be called if the future fails.
      */
     void forEach(Consumer<? super T> action);
 
     /**
-     * Creates a new future by applying the 's' function to the successful result of
-     * this result, or the 'f' function to the failed result. If there is any non-fatal
-     * exception thrown when 's' or 'f' is applied, that exception will be propagated
+     * Creates a new future by applying the transformer to the successful result of
+     * this future, or the failure to the failed result. If there is any non-fatal
+     * exception thrown when transformer or failure is applied, that exception will be propagated
      * to the resulting future.
-     *
-     * @param s function that transforms a successful result of the receiver into a
-     *          successful result of the returned result
-     * @param f function that transforms a failure of the receiver into a failure of
-     *          the returned result
-     * @return a result that will be completed with the transformed value
      */
     <R> DeferredResult<R> transform(Function<? super T, ? extends R> transformer,
-                                    Function<? super Throwable, ? super Throwable> f);
+                                    Function<? super Throwable, ? super Throwable> failure);
 
     /**
-     * Creates a new result by applying a function to the successful result of
-     * this one. If this result is completed with an exception then the new
-     * result will also contain this exception.
+     * Creates a new future by applying a function to the successful result of
+     * this future. If this future is completed with an exception then the new
+     * future will also contain this exception.
      */
     <R> DeferredResult<R> map(Function<? super T, ? extends R> mapper);
 
@@ -138,9 +130,9 @@ public interface DeferredResult<T> {
     DeferredResult<Long> mapToLong(ToLongFunction<? super T> mapper);
 
     /**
-     * Creates a new result by applying a function to the successful result of
-     * this result, and returns the result of the function as the new result.
-     * If this result is completed with an exception then the new result will
+     * Creates a new future by applying a function to the successful result of
+     * this future, and returns the result of the function as the new future.
+     * If this future is completed with an exception then the new future will
      * also contain this exception.
      */
     <R> DeferredResult<R> flatMap(Function<? super T, ? extends DeferredResult<? extends R>> mapper);
@@ -152,17 +144,17 @@ public interface DeferredResult<T> {
     DeferredResult<Long> flatMapToLong(Function<? super T, ? extends DeferredResult<Long>> mapper);
 
     /**
-     * Creates a new result by filtering the value of the current result with a predicate.
+     * Creates a new future by filtering the value of the current future with a predicate.
      * <p/>
-     * If the current result contains a value which satisfies the predicate, the new result will also hold that value.
-     * Otherwise, the resulting result will fail with a `NoSuchElementException`.
+     * If the current future contains a value which satisfies the predicate, the new future will also hold that value.
+     * Otherwise, the resulting future will fail with a `NoSuchElementException`.
      * <p/>
-     * If the current result fails, then the resulting result also fails.
+     * If the current future fails, then the resulting future also fails.
      */
     DeferredResult<T> filter(Predicate<? super T> predicate);
 
     /**
-     * Performs a mutable reduction operation on the elements of this stream. A mutable reduction is one in which
+     * Performs a mutable reduction operation on the future. A mutable reduction is one in which
      * the reduced value is a mutable value holder, such as an ArrayList, and elements are incorporated by
      * updating the state of the result, rather than by replacing the result.
      */
@@ -170,7 +162,42 @@ public interface DeferredResult<T> {
                   BiConsumer<R, ? super T> accumulator,
                   BiConsumer<R, R> combiner);
 
-    // TODO: Add:
-    // instance methods: recover, recoverWith, zip, fallbackTo, mapTo, andThen
-    // static methods: firstCompletedOf, find, fold, reduce, traverse
+    /**
+     * Creates a new future that will handle any matching throwable that this
+     * future might contain. If there is no match, or if this future contains
+     * a valid result then the new future will contain the same.
+     */
+    <R> DeferredResult<R> recover(Function<? super Throwable, ? extends R> recovery);
+
+    <R> DeferredResult<R> recoverWith(Function<? super Throwable, DeferredResult<? extends R>> recovery);
+
+    // No zip method as there are no tuples in Java.
+
+    /**
+     * Creates a new future which holds the result of this future if it was completed successfully, or, if not,
+     * the result of the `that` future if `that` is completed successfully.
+     * If both futures are failed, the resulting future holds the throwable object of the first future.
+     * <p/>
+     * Using this method will not cause concurrent programs to become nondeterministic.
+     */
+    <R extends T> DeferredResult<R> fallbackTo(Function<? super T, ? extends R> fallback);
+
+    // No mapTo method as ClassTags are not available in Java.
+
+    /**
+     * Applies the side-effecting function to the result of this future, and returns
+     * a new future with the result of this future.
+     * <p/>
+     * This method allows one to enforce that the callbacks are executed in a
+     * specified order.
+     * <p/>
+     * Note that if one of the chained `andThen` callbacks throws
+     * an exception, that exception is not propagated to the subsequent `andThen`
+     * callbacks. Instead, the subsequent `andThen` callbacks are given the original
+     * value of this future.
+     */
+    DeferredResult<T> andThen(Consumer<? super T> action);
+
 }
+// TODO: Add:
+// static methods: firstCompletedOf, find, fold, reduce, traverse
