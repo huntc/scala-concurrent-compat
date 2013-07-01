907da62aa010c3a7d0d1197523090d6d7f9362fb
Christopher Hunt <huntchr@gmail.com>
2013-07-01 21:49:45 +1000

    initial commit - test isn't running for some reason

--- build.sbt
+++ build.sbt
@@ -5,3 +5,7 @@ name := "scala-concurrent-compat"
 version := "1.0.0-SNAPSHOT"
 
 scalaVersion := "2.10.2"
+
+libraryDependencies += "com.typesafe.akka" % "akka-actor_2.10" % "2.2.0-RC1"
+
+libraryDependencies += "com.novocode" % "junit-interface" % "0.10-M4" % "test"
---
+++ src/test/java/SomeTest.java
@@ -0,0 +1,41 @@
+import akka.actor.ActorSystem;
+import akka.dispatch.Futures;
+import akka.japi.Procedure;
+import org.junit.Test;
+import scala.concurrent.Future;
+import scala.concurrent.Promise;
+
+public class SomeTest {
+    @Test
+    public void testSomeFutures() {
+        ActorSystem system = ActorSystem.create("MySystem");
+
+        final Promise<Integer> p = Futures.promise();
+        final Future<Integer> f = p.future();
+
+        f.onSuccess(OnSuccess.of((Integer v) -> {
+            System.out.println(v);
+        }), system.dispatcher());
+
+        p.success(1);
+    }
+
+    public final static class OnSuccess<T> extends akka.dispatch.OnSuccess<T> {
+
+        public static <U> OnSuccess of(final Procedure<U> body) {
+            return new OnSuccess(body);
+        }
+
+        final Procedure body;
+
+        protected OnSuccess(final Procedure<T> body) {
+            this.body = body;
+        }
+
+        @Override
+        public final void onSuccess(T t) throws Exception {
+            body.apply(t);
+        }
+    }
+
+}
